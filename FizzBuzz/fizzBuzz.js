b8f899e335afd7e30cb4786da9c0ff2f866fdd58
Harry <harishusagicxy@gmail.com>
2021-02-10 15:07:24 +0100

    A few additional takes on the FizzBuzz

--- FizzBuzz/fizzBuzz.js
+++ FizzBuzz/fizzBuzz.js
@@ -1,24 +1,56 @@
 document.querySelector('#press').addEventListener('click', fizzBuzz)
 
-
-let output = "";
-
+let inputNum = document.querySelector('#yourNum').value
+let output;
 
 function fizzBuzz() {
 
-    let inputNum = Number(document.querySelector('#yourNum').value)
-
-    for (let i = 0; inputNum < i; i++) {
-
-
-        if (inputNum % 3 == 0) { output = +"Fizz" }
-        if (inputNum % 3 == 0) { output = +"Buzz" }
-        if (output == "") { output = i }
-
-    }
-
-document.querySelector('#zebra').innerText = `${output}`
+    for (var i=1; i <= inputNum; i++)
+{
+    if (i % 15 == 0)
+        console.log("FizzBuzz");
+    else if (i % 3 == 0)
+        console.log("Fizz");
+    else if (i % 5 == 0)
+        console.log("Buzz");
+    else
+        console.log(i);
 }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// 
+// var output = ""; 
+// 
+// 
+// function fizzBuzz() {
+// 
+    // let inputNum = document.querySelector('#yourNum').value
+// 
+    // for (let i=0; i < inputNum; i++) {
+// 
+// 
+        // if (i % 3 == 0) { output += "Fizz" }
+        // if (i % 5 == 0) { output += "Buzz" }
+        // if (output == "") { output = i }
+// 
+    // }
+// 
+// document.querySelector('#zebra').innerText = `${output}`
+// 
+// console.log(inputNum);
+// console.log(output);
+// }
 // for (let i = 1; i <= 100; i++) {
 
     // let meho = "";
